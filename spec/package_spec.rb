7f6a51a00580cc25e7833be937f85f94a408f5c4
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2013-02-17 19:40:20 -0800

    explicitly set cookbook_path into the ChefRunner

--- spec/package_spec.rb
+++ spec/package_spec.rb
@@ -1,7 +1,7 @@
 require 'chefspec'
 
 describe 'bsdcpio default' do
-  let(:chef_run) { ChefSpec::ChefRunner.new.converge 'bsdcpio::default' }
+  let(:chef_run) { ChefSpec::ChefRunner.new(cookbook_path: File.expand_path('../../..', __FILE__)).converge 'bsdcpio::default' }
   it 'installs bsdcpio' do
     chef_run.should install_package 'bsdcpio'
   end
