01a79482b07e873bb913e98f050a0719ac7e42c2
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2015-07-21 16:57:50 -0700

    update metadata with chef-brigade contact info

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
-name             'bsdcpio'
-maintainer       'Joseph Anthony Pasquale Holsten'
-maintainer_email 'joseph@josephholsten.com'
-license          'Apache 2.0'
-description      'Installs bsdcpio'
-version          '1.0.0'
+name 'bsdcpio'
+source_url "https://github.com/chef-brigade/#{name}-cookbook" if respond_to?(:source_url)
+issues_url "https://github.com/chef-brigade/#{name}-cookbook/issues" if respond_to?(:issues_url)
+maintainer 'Chef Brigade'
+maintainer_email 'help@chefbrigade.io'
+license 'Apache 2.0'
+description 'Installs bsdcpio'
+version '1.0.0'
 
 recipe 'bsdcpio::default', 'Installs bsdcpio package'
 
