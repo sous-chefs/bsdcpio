6cad1b4766ac0e3eb158df39584ab1b0624ba9bc
Tim Smith <tsmith@chef.io>
2018-05-08 15:23:04 -0700

    Use a SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ issues_url       'https://github.com/sous-chefs/bsdcpio/issues'
 maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 chef_version     '>= 12.1'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs bsdcpio'
 version          '1.0.0'
 
