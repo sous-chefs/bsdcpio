80ad78e8e5dc479d847979213d2f5cdb33495bc4
Tim Smith <tsmith@chef.io>
2019-07-26 01:07:58 -0700

    Use chef standard comment headers (#22)
    Autocorrected by Cookstyle
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Cookbook:: bsdcpio
 # Resource:: default
 #
-# Copyright 2013, Joseph Anthony Pasquale Holsten
+# Copyright:: 2013, Joseph Anthony Pasquale Holsten
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
